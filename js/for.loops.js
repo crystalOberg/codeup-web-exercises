351332c87bc6cd0370d35d586b4a35fb362be4d5
Crystal <crystal.ochoa03@gmail.com>
2022-11-17 23:20:31 +0100

    jquery_exercises

--- js/for.loops.js
+++ js/for.loops.js
@@ -1,51 +1,55 @@
 "use script"
 
-var x = 7;
+// var x = 7;
+//
+// function showMultiplicationTable(x) {
+// }
+//     for (i = 1; i < 11; i++) {
+//
+//         console.log(`7 * ${i} = ` + (i * x));
+//
+//
+//     }
+//         console.log(showMultiplicationTable(7));
+//
+//
+//     function getRandomArbitrary(min, max) {
+//         return Math.floor(math.random() * max - min) + min;
+//
+//     console.log(getRandomArbitrary(20, 200));
+//
+//     if (randNum % 2 === 0) {
+//         console.log (x + " is even");
+//     }
+//     else {
+//     console.log (x + " is odd");
+//     }
+// }
+//
+//
+//     let output = '';
+//     for (let i = 1; i < 10; i++) {
+//         for (let j = 0; j < i; j++) {
+//             output += 1;
+//         }
+//         console.log(output);
+// }
+//
+//
+// var x = 5;
+//
+// function multiplyByFive() {
+// }
+//     for (let i = 100; i > 1; i--) {
+//         if (i % 5 === 0) {
+//
+//             console.log(i);
+//         }
+//     }
+//
+//     multiplyByFive();
 
-function showMultiplicationTable(x) {
+var i = 10;
+while(i < 10) {
+    console.log(i);
 }
-    for (i = 1; i < 11; i++) {
-
-        console.log(`7 * ${i} = ` + (i * x));
-
-
-    }
-        console.log(showMultiplicationTable(7));
-
-
-    function getRandomArbitrary(min, max) {
-        return Math.floor(math.random() * max - min) + min);
-
-    console.log(getRandomArbitrary(20, 200));
-
-    if (randNum % 2 === 0) {
-        console.log (x + " is even");
-    }
-    else {
-    console.log (x + " is odd");
-    }
-}
-
-
-    let output = '';
-    for (let i = 1; i < 10; i++) {
-        for (let j = 0; j < i; j++) {
-            output += 1;
-        }
-        console.log(output);
-}
-
-
-var x = 5;
-
-function multiplyByFive() {
-}
-    for (let i = 100; i > 1; i--) {
-        if (i % 5 === 0) {
-
-            console.log(i);
-        }
-    }
-
-    multiplyByFive();
-
