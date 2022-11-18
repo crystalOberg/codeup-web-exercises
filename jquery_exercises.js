c0b6dd703e062178bb65c64f95c7558f3cdee615
Crystal <crystal.ochoa03@gmail.com>
2022-11-18 16:54:55 +0100

    completed the events exercise

--- jquery_exercises.js
+++ jquery_exercises.js
@@ -1,10 +1,25 @@
-$('*').css('border', '1px solid #9A1B1E');
+"use strict"
+//
+// $(function() {
+//     alert($("#codeup").html());
+//     alert($("#main").html());
+//
+//     $('*').css('border', '1px solid #9A1B1E');
+//
+//     $("li").css("font-size", "20px").css("background-color", "yellow");
+//
+//     $("h2").css("background-color", "yellow");
+//     $("p").css("background-color", "yellow");
+//     alert($("h1").html());
+//
+//     $("h1, p, li").css("background-color", "yellow");
+//
+// });
 
-    $(document).ready(function(){
+$(document).ready(function(){
     $("li").hover(function(){
         $(this).css("color", "red");
     }, function(){
         $(this).css("color", "black");
     });
 });
-
