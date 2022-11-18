47b732c15aff888cbb0f0a869ad0e2fcd942a005
Crystal <crystal.ochoa03@gmail.com>
2022-11-18 18:06:52 +0100

    completed selectors exercise

--- jquery_exercises.js
+++ jquery_exercises.js
@@ -1,38 +1,38 @@
 "use strict"
-//
-// $(function() {
-//     alert($("#codeup").html());
-//     alert($("#main").html());
-//
-//     $('*').css('border', '1px solid #9A1B1E');
-//
-//     $("li").css("font-size", "20px").css("background-color", "yellow");
-//
-//     $("h2").css("background-color", "yellow");
-//     $("p").css("background-color", "yellow");
-//     alert($("h1").html());
-//
-//     $("h1, p, li").css("background-color", "yellow");
-//
+
+$(function() {
+    alert($("#codeup").html());
+    alert($("#main").html());
+
+    $('*').css('border', '1px solid #9A1B1E');
+
+    $("li").css("font-size", "20px").css("background-color", "yellow");
+
+    $("h2").css("background-color", "yellow");
+    $("p").css("background-color", "yellow");
+    alert($("h1").html());
+
+    $("h1, p, li").css("background-color", "yellow");
+
+});
+// $('document').ready(function(){
+//     $('li').hover(
+//         function() {
+//             $(this).css('color', 'red');
+//         },
+//         function() {
+//             $(this).css('color', 'black');
+//         })
 // });
-$('document').ready(function(){
-    $('li').hover(
-        function() {
-            $(this).css('color', 'red');
-        },
-        function() {
-            $(this).css('color', 'black');
-        })
-});
-
-$('document').ready(function(){
-$("#lights-out").click(function() {
-    $("h1").css("background-color", "black");
-})
-});
-
-$('document').ready(function(){
-    $("#size").dblclick(function() {
-        $("p").css("font-size", "18px");
-    })
-});
+//
+// $('document').ready(function(){
+// $("#lights-out").click(function() {
+//     $("h1").css("background-color", "black");
+// })
+// });
+//
+// $('document').ready(function(){
+//     $("#size").dblclick(function() {
+//         $("p").css("font-size", "18px");
+//     })
+// });
