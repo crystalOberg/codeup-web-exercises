ff4496a770192b23886c2a21acfafd0da1ad4470
Crystal <crystal.ochoa03@gmail.com>
2022-11-30 23:59:14 +0100

    completed mapbox_maps_api exercise

---
+++ mapbox_maps_api.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// for (const feature of geojson.features) {
+//     // create a HTML element for each feature
+//     const el = document.createElement('div');
+//     el.className = 'marker';
+//
+//     // make a marker for each feature and add to the map
+//     new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
+// }
+// new mapboxgl.Marker(el)
+//     .setLngLat(feature.geometry.coordinates)
+//     .setPopup(
+//         new mapboxgl.Popup({ offset: 25 }) // add popups
+//             .setHTML(
+//                 `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+//             )
+//     )
+//     .addTo(map);
