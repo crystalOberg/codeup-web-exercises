96f5121113a06d29f77f6eb8eee7cc028c3dd36d
Crystal <crystal.ochoa03@gmail.com>
2022-12-11 19:41:32 +0100

    selectors exercise

--- jquery_exercises.js
+++ jquery_exercises.js
@@ -1,35 +1,35 @@
 "use strict"
 
-// $(function() {
-//     alert($("#codeup").html());
-//     alert($("#main").html());
-//
-//     $('*').css('border', '1px solid #9A1B1E');
-//
-//     $("li").css("font-size", "20px").css("background-color", "yellow");
-//
-//     $("h2").css("background-color", "yellow");
-//     $("p").css("background-color", "yellow");
-//     alert($("h1").html());
-//
-//     $("h1, p, li").css("background-color", "yellow");
-//
-// });
-$('document').ready(function(){
-    $('li').hover(
-        function() {
-            $(this).css('color', 'red');
-        },
-        function() {
-            $(this).css('color', 'black');
-        });
+$(function() {
+    alert($("#codeup").html());
+    alert($("#main").html());
 
-    $("#lights-out").click(function() {
-        $("h1").css("background-color", "black");
-    })
+    $('*').css('border', '1px solid #9A1B1E');
 
-    $("#size").dblclick(function() {
-        $("p").css("font-size", "18px");
-    })
+    $("li").css("font-size", "20px").css("background-color", "yellow");
+
+    $("h2").css("background-color", "yellow");
+    $("p").css("background-color", "yellow");
+    alert($("h1").html());
+
+    $("h1, p, li").css("background-color", "yellow");
+
 });
+// $('document').ready(function(){
+//     $('li').hover(
+//         function() {
+//             $(this).css('color', 'red');
+//         },
+//         function() {
+//             $(this).css('color', 'black');
+//         });
+//
+//     $("#lights-out").click(function() {
+//         $("h1").css("background-color", "black");
+//     })
+//
+//     $("#size").dblclick(function() {
+//         $("p").css("font-size", "18px");
+//     })
+// });
 
